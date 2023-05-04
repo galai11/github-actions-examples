76c26fd91452cad05609d810cbb29ce7b074c627
unknown <galaijihed94@gmail.com~>
2023-05-04 12:16:54 +0200

    Initial commit

---
+++ src/main/java/com/galaijihed/githubcicdactions/GithubCicdActionsApplication.java
@@ -0,0 +1,25 @@
+package com.galaijihed.githubcicdactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+	public String message(){
+		return "Welcome to CICD imlementation"
+	}
+/*
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/galai11/github-actions-examples.git
+git push -u origin main
+ */
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
